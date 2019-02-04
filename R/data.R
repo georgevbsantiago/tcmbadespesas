5ea29c8aed7a7ce9101480447e3eaf509196cb9e
George Santiago <georgevbsantiago@hotmail.com>
2019-02-04 21:56:44 -0200

    Acrescido o dataset 'tcm_cod_municipios'

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Codigo dos Municipios
+#'
+#' Dataset contendo o código dos municípios para servir de referência no Web Scrapingstr()
+#'
+#' @format A data frame with 996 rows and 4 variables:
+#' \describe{
+#'   \item{cod_municipio}{Codigo do Municipio}
+#'   \item{nm_municipio}{Nome do Municipio}
+#'   \item{cod_entidade}{Codigo do ente municipal - Nao usar esse numero do Web Scraping}
+#'   \item{nm_entidade}{Codigo do ente municipal}
+#'   ...
+#' }
+#' @source \url{http://www.tcm.ba.gov.br/}
+"tcm_cod_municipios"
